cbff4e67adbfc5e8b33ee1a66a8f24ef81c0a957
Soyeb <tssoyeb@gmail.com>
2022-04-30 03:28:58 +0600

    added supplier section to the home

--- src/component/Home/Home/Home.js
+++ src/component/Home/Home/Home.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
 import Products from '../Products/Products';
+import Suppliers from '../Suppliers/Suppliers';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
-            
+
             <Products></Products>
+            <div className='mt-4'>
+               
+                <Suppliers></Suppliers>
+
+            </div>
         </div>
     );
 };
---
+++ src/component/Home/Supplier/Supplier.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Supplier = ({info}) => {
+    return (
+        <div className='col gy-4 gx-2 shadow rounded p-3 mb-2 bg-body'>
+        <div className="card shadow rounded mb-2 bg-body">
+            
+            <div className="card-body">
+                <h5 className="card-title">{info.name}</h5>
+                <p className="">Address : {info.address} </p>
+                <p className="">COntact No. : {info.phone} </p>
+            </div>
+            {/* <div className="card-footer d-block text-center">
+                <button onClick={() => handleStockUpdate(_id)} className='btn btn-info '> Stock Update</button>
+            </div> */}
+        </div>
+    </div>
+    );
+};
+
+export default Supplier;
