604da60a3d3e1f6b44e9307b60c014b2c32cc4f5
Soyeb <tssoyeb@gmail.com>
2022-05-08 13:27:31 +0600

    route privating

--- src/App.js
+++ src/App.js
@@ -23,25 +23,40 @@ function App() {
         <Route path='/home' element={<Home></Home>}></Route>
 
         <Route path='/inventory/:id' element={
-        <RequireAuth>
-          <Inventory></Inventory>
-        </RequireAuth>}>
+          <RequireAuth>
+            <Inventory></Inventory>
+          </RequireAuth>}>
         </Route>
 
         <Route path='/blogs' element={<Blogs></Blogs>}></Route>
         <Route path='/login' element={<Login></Login>}></Route>
         <Route path='/register' element={<Register></Register>}></Route>
-        <Route path='/manage' element={<ManageInventory></ManageInventory>}></Route>
-        <Route path='/addItem' element={<AddNewItem></AddNewItem>}></Route>
-        <Route path='/myItems' element={<MyItems></MyItems>}></Route>
-        <Route path='*' element={<NotFound></NotFound>}></Route>
-        
 
-        
+        <Route path='/manage' element={
+          <RequireAuth>
+            <ManageInventory></ManageInventory>
+          </RequireAuth>}>
+        </Route>
+
+        <Route path='/addItem' element={
+          <RequireAuth>
+            <AddNewItem></AddNewItem>
+          </RequireAuth>}>
+        </Route>
+
+        <Route path='/myItems' element={
+          <RequireAuth>
+            <MyItems></MyItems>
+          </RequireAuth>}>
+        </Route>
+        <Route path='*' element={<NotFound></NotFound>}></Route>
+
+
+
 
       </Routes>
       <Footer></Footer>
-      
+
     </div>
   );
 }
